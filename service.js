7f72aa229f709222dea828e2ab8b433739319b52
KhushbooVarathe <khushboo.va@cisinlabs.com>
2023-08-22 15:19:58 +0530

    message

--- service.js
+++ service.js
@@ -7,9 +7,11 @@ var cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static(path.join(__dirname,'./client/build')))
 
-
-
+app.get('*',function(req,res){
+    res.send(path.join(__dirname,'./client/build/index.html'))
+})
 app.use('/api',auth)
 app.listen(7005,()=>{
     console.log("api running at port 7005")
