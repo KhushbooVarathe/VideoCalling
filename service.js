8fec235bbf45a99595af6396d3465e6e5e86979d
KhushbooVarathe <khushboo.va@cisinlabs.com>
2023-08-22 17:52:02 +0530

    message

--- service.js
+++ service.js
@@ -8,11 +8,12 @@ var cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname, '../Deploy/client/build')));
 
-app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
-})
+// If a request doesn't match any static file, serve the "index.html" file
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../Deploy/client/build/index.html'));
+});
 app.use('/api',auth)
 app.listen(7005,()=>{
     console.log("api running at port 7005")
